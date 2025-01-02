a5b3730014b880b3f00eae01612c7366de3047bd
Mahfuz21-04050 <u2104050@student.cuet.ac.bd>
2025-01-02 14:02:40 +0600

    width update 50% from100%

--- src/main/java/com/example/Model/user.java
+++ src/main/java/com/example/Model/user.java
@@ -14,9 +14,7 @@ public class user {
     private  String email;
     private  String password;
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() {return id ;}
 
     public void setId(Long id) {
         this.id = id;
--- src/main/resources/static/filter.js
+++ src/main/resources/static/filter.js
@@ -1,8 +1,9 @@
-//   const userId = [[${user_id}]]; // Default to '0' if not set
-//        console.log( userId);
+     // const userId = [[${user_id}]]; // Default to '0' if not set
+     //  console.log( userId);
        var userId = document.getElementById("logged-in-user-id").value;
-        console.log(`UserId: ${userId}`);
-        document.addEventListener('DOMContentLoaded', function() { loadAllTransactions(); });
+      console.log(`UserId: ${userId}`);
+
+      document.addEventListener('DOMContentLoaded', function() { loadAllTransactions(); });
         function loadAllTransactions()
          { fetch(`/filterTransactions?user_id=${userId}`)
           .then(response => response.json())
@@ -20,6 +21,20 @@
               })
               .catch((error) => { console.error('There was a problem with your fetch operation:', error); });
               }
+               function showFilters() {
+                          const filters = document.getElementById('filters');
+                          const submitBtn = document.getElementById('submit-btn');
+                          filters.style.display = 'flex';
+                          submitBtn.style.display = 'block';
+                      }
+
+                       function TshowFilters() {
+                                                const Tfilters = document.getElementById('table-filters');
+                                                const TsubmitBtn = document.getElementById('Tsubmit-btn');
+                                                Tfilters.style.display = 'flex';
+                                                TsubmitBtn.style.display = 'block';
+                                            }
+
 
 
         function filterTable(){
@@ -45,3 +60,12 @@
                     });
                 });
         }
+
+
+
+
+
+
+
+
+
